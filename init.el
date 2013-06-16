54a633b0b57bed87296ab08e2d816ee5d5686712
danny <daniel.slutsky@gmail.com>
2013-06-16 12:25:01 +0300

    display issues and scrolling

--- init.el
+++ init.el
@@ -498,7 +498,7 @@
     ;;(set-face-background face-symbol (color-darken-name "black" (- (random 20))))
     (setq rgb (mapcar 
 	       (function (lambda (x) (let 
-                                    ((y (* 3 (+ x (/ (- (random 100) 50) 900.0)))))
+                                    ((y (* 1.2 (+ x (/ (- (random 100) 50) 900.0)))))
                                   (if 
                                       (> y 1) 
                                       1 ;;(- 2 y)
@@ -525,6 +525,10 @@
 	    (default-value 'mode-line-format)
 	  nil))
   (redraw-display))
+
+
+(set-fringe-style '(0 . 0))
+
 
 
 ;;;;;;;; MODE-LINE
@@ -706,30 +710,38 @@
 
 ;;;;;;;; SCROLLING
 
+;; http://www.emacswiki.org/emacs/SmoothScrolling
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+    
+(setq scroll-step 1) ;; keyboard scroll one line at a time
 
-;; http://web.archive.org/web/20061025212623/http://www.cs.utexas.edu/users/hllu/EmacsSmoothScrolling.html
-(setq default-truncate-lines t)
-(defun point-of-beginning-of-bottom-line ()
-  (save-excursion
-    (move-to-window-line -1)
-    (point)))
-(defun point-of-beginning-of-line ()
-  (save-excursion
-    (beginning-of-line)
-    (point)))
-(defun next-one-line () (interactive)
-  (if (= (point-of-beginning-of-bottom-line) (point-of-beginning-of-line))
-      (progn (scroll-up 1)
-             (next-line 1))
-    (next-line 1)))
-(defun point-of-beginning-of-top-line ()
-  (save-excursion
-    (move-to-window-line 0)
-    (point)))
-(defun previous-one-line () (interactive)
-  (if (= (point-of-beginning-of-top-line) (point-of-beginning-of-line))
-      (progn (scroll-down 1)
-             (previous-line 1))
-    (previous-line 1)))
-(global-set-key (kbd "<down>") 'next-one-line)
-(global-set-key (kbd "<up>") 'previous-one-line)
+
+;; ;; http://web.archive.org/web/20061025212623/http://www.cs.utexas.edu/users/hllu/EmacsSmoothScrolling.html
+;; (setq default-truncate-lines t)
+;; (defun point-of-beginning-of-bottom-line ()
+;;   (save-excursion
+;;     (move-to-window-line -1)
+;;     (point)))
+;; (defun point-of-beginning-of-line ()
+;;   (save-excursion
+;;     (beginning-of-line)
+;;     (point)))
+;; (defun next-one-line () (interactive)
+;;   (if (= (point-of-beginning-of-bottom-line) (point-of-beginning-of-line))
+;;       (progn (scroll-up 1)
+;;              (next-line 1))
+;;     (next-line 1)))
+;; (defun point-of-beginning-of-top-line ()
+;;   (save-excursion
+;;     (move-to-window-line 0)
+;;     (point)))
+;; (defun previous-one-line () (interactive)
+;;   (if (= (point-of-beginning-of-top-line) (point-of-beginning-of-line))
+;;       (progn (scroll-down 1)
+;;              (previous-line 1))
+;;     (previous-line 1)))
+;; (global-set-key (kbd "<down>") 'next-one-line)
+;; (global-set-key (kbd "<up>") 'previous-one-line)
