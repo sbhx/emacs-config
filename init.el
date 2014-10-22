88b11a8751e6e37ce73b859a0427a3f98b3dc444
daslu <daniel.slutsky@gmail.com>
2014-10-22 19:34:26 +0300

    added a commnted-out draft of evaluation commands

--- init.el
+++ init.el
@@ -873,6 +873,100 @@ want to use in the modeline *in lieu of* the original.")
 ;; (setq ensime-inf-default-cmd-line '("sbt" "console"))
 
 
+;;;;;;;; REPL EVAL
+
+;; ;; http://kazyos.hatenablog.com/entry/2014/04/13/094951
+
+;; (defun my-repl-eval (repl-buffer-name fun-repl-start fun-repl-send defun-string)
+;;     "Evaluates expression using a REPL specified by repl-buffer-name. Sends
+;; expression using a function specified in fun-repl-start. A function definition
+;;  is detected by a string specified in defun-string and handled accordingly."
+;;   (interactive)
+;;   (let* (;; Save current point
+;; 	 (initial-point (point)))
+;;     ;; defined in 200_my-misc-functions-and-bindings.el
+;;     (my-repl-start repl-buffer-name fun-repl-start)
+;;     ;; Check if selection is present
+;;     (if (and transient-mark-mode mark-active)
+;; 	;; If selected, send to ielm
+;; 	(funcall fun-repl-send (point) (mark))
+;;       ;; If not selected, do all the following
+;;       ;; Move to the beginning of line
+;;       (beginning-of-line)
+;;       ;; Check if the first word is def (function def)
+;;       (if (looking-at defun-string)
+;; 	  ;; Use eval-defun if on defun
+;; 	  (progn
+;; 	    ;; Set a mark there
+;; 	    (set-mark (line-beginning-position))
+;; 	    ;; Go to the end
+;; 	    (forward-sexp)
+;; 	    ;; Send to ielm
+;; 	    (funcall fun-repl-send (point) (mark))
+;; 	    ;; Go to the next expression
+;; 	    (forward-sexp))
+;; 	;; If it is not def, do all the following
+;; 	;; Go to the previous position
+;; 	(goto-char initial-point)
+;; 	;; Go back one S-exp. (paredit dependency)
+;; 	(paredit-backward)
+;; 	;; Loop
+;; 	(while (not (equal (current-column) 0))
+;; 	  ;; Go back one S-exp. (paredit dependency)
+;; 	  (paredit-backward))
+;; 	;; Set a mark there
+;; 	(set-mark (line-beginning-position))
+;; 	;; Go to the end of the S-exp starting there
+;; 	(forward-sexp)
+;; 	;; Eval the S-exp before
+;; 	(funcall fun-repl-send (point) (mark))
+;; 	;; Go to the next expression
+;; 	(forward-sexp)
+;; 	))))
+
+;; (defun not-implemented ()
+;;   (interactive)
+;;   (shell-command "notify-send not implemented"))
+
+;; (defun my-send-clojure (start end)
+;;     "Sends expression to *cider-repl* and have it evaluated."
+
+;;   (let* (;; Assign the current buffer
+;; 	 (script-window (selected-window))
+;; 	 ;; Assign the region as a string
+;; 	 (region-string (buffer-substring-no-properties start end)))
+
+;;     ;; Change to cider REPL
+;;     (cider-switch-to-repl-buffer)
+;;     ;; Move to end of buffer
+;;     (end-of-buffer)
+;;     ;; ;; Set mark from beginning
+;;     ;; (set-mark (line-beginning-position))
+;;     ;; ;; Delete the region
+;;     ;; (delete-region (point) (mark))
+;;     ;; ;; Unset transient mark
+;;     ;; (setq mark-active nil)
+;;     ;; Insert the string
+;;     (insert region-string)
+;;     ;; Execute
+;;     (cider-repl-return)
+;;     ;; Come back to the script
+;;     (select-window script-window)
+;;     ;; Return nil
+;;     nil
+;;     ))
+
+;; (my-repl-eval
+;;  ;; repl-buffer-name
+;;  "*shell*"
+;;  ;; fun-repl-start
+;;  #'note-implemented
+;;  ;; fun-repl-send
+;;  #'my-send-clojure
+;;  ;;defun-string
+;;  "(defn ")
+;; ;;
+
 ;;;;;;;; CLOJURE COOKBOOK
 
 (defun increment-clojure-cookbook ()
